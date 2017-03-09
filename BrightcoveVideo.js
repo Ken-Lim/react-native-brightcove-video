94edf963cd4651453f93d7a3e199137adfb899b8
John Clema <john.clema@wanews.com.au>
2017-03-09 11:25:31 +0800

    Updates for basic brightcove to run on ios

--- BrightcoveVideo.js
+++ BrightcoveVideo.js
@@ -3,238 +3,238 @@ import {StyleSheet, requireNativeComponent, NativeModules, View, Image} from 're
 import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource';
 import VideoResizeMode from './VideoResizeMode.js';
 
-const styles = StyleSheet.create({
-  base: {
-    overflow: 'hidden',
-  },
-});
+// const styles = StyleSheet.create({
+//   base: {
+//     overflow: 'hidden',
+//   },
+// });
 
 export default class BrightcoveVideo extends Component {
 
   constructor(props) {
     super(props);
 
-    this.state = {
-      showPoster: true,
-    };
+    // this.state = {
+    //   showPoster: true,
+    // };
   }
 
   setNativeProps(nativeProps) {
     this._root.setNativeProps(nativeProps);
   }
 
-  seek = (time) => {
-    this.setNativeProps({ seek: time });
-  };
+  // seek = (time) => {
+  //   this.setNativeProps({ seek: time });
+  // };
 
-  presentFullscreenPlayer = () => {
-    this.setNativeProps({ fullscreen: true });
-  };
+  // presentFullscreenPlayer = () => {
+  //   this.setNativeProps({ fullscreen: true });
+  // };
 
-  dismissFullscreenPlayer = () => {
-    this.setNativeProps({ fullscreen: false });
-  };
+  // dismissFullscreenPlayer = () => {
+  //   this.setNativeProps({ fullscreen: false });
+  // };
 
   _assignRoot = (component) => {
     this._root = component;
   };
 
-  _onLoadStart = (event) => {
-    if (this.props.onLoadStart) {
-      this.props.onLoadStart(event.nativeEvent);
-    }
-  };
+  // _onLoadStart = (event) => {
+  //   if (this.props.onLoadStart) {
+  //     this.props.onLoadStart(event.nativeEvent);
+  //   }
+  // };
 
-  _onLoad = (event) => {
-    if (this.props.onLoad) {
-      this.props.onLoad(event.nativeEvent);
-    }
-  };
+  // _onLoad = (event) => {
+  //   if (this.props.onLoad) {
+  //     this.props.onLoad(event.nativeEvent);
+  //   }
+  // };
 
-  _onError = (event) => {
-    if (this.props.onError) {
-      this.props.onError(event.nativeEvent);
-    }
-  };
+  // _onError = (event) => {
+  //   if (this.props.onError) {
+  //     this.props.onError(event.nativeEvent);
+  //   }
+  // };
 
-  _onProgress = (event) => {
-    if (this.props.onProgress) {
-      this.props.onProgress(event.nativeEvent);
-    }
-  };
+  // _onProgress = (event) => {
+  //   if (this.props.onProgress) {
+  //     this.props.onProgress(event.nativeEvent);
+  //   }
+  // };
 
-  _onSeek = (event) => {
-    if (this.state.showPoster) {
-      this.setState({showPoster: false});
-    }
+  // _onSeek = (event) => {
+  //   if (this.state.showPoster) {
+  //     this.setState({showPoster: false});
+  //   }
 
-    if (this.props.onSeek) {
-      this.props.onSeek(event.nativeEvent);
-    }
-  };
+  //   if (this.props.onSeek) {
+  //     this.props.onSeek(event.nativeEvent);
+  //   }
+  // };
 
-  _onEnd = (event) => {
-    if (this.props.onEnd) {
-      this.props.onEnd(event.nativeEvent);
-    }
-  };
+  // _onEnd = (event) => {
+  //   if (this.props.onEnd) {
+  //     this.props.onEnd(event.nativeEvent);
+  //   }
+  // };
 
-  _onFullscreenPlayerWillPresent = (event) => {
-    if (this.props.onFullscreenPlayerWillPresent) {
-      this.props.onFullscreenPlayerWillPresent(event.nativeEvent);
-    }
-  };
+  // _onFullscreenPlayerWillPresent = (event) => {
+  //   if (this.props.onFullscreenPlayerWillPresent) {
+  //     this.props.onFullscreenPlayerWillPresent(event.nativeEvent);
+  //   }
+  // };
 
-  _onFullscreenPlayerDidPresent = (event) => {
-    if (this.props.onFullscreenPlayerDidPresent) {
-      this.props.onFullscreenPlayerDidPresent(event.nativeEvent);
-    }
-  };
+  // _onFullscreenPlayerDidPresent = (event) => {
+  //   if (this.props.onFullscreenPlayerDidPresent) {
+  //     this.props.onFullscreenPlayerDidPresent(event.nativeEvent);
+  //   }
+  // };
 
-  _onFullscreenPlayerWillDismiss = (event) => {
-    if (this.props.onFullscreenPlayerWillDismiss) {
-      this.props.onFullscreenPlayerWillDismiss(event.nativeEvent);
-    }
-  };
+  // _onFullscreenPlayerWillDismiss = (event) => {
+  //   if (this.props.onFullscreenPlayerWillDismiss) {
+  //     this.props.onFullscreenPlayerWillDismiss(event.nativeEvent);
+  //   }
+  // };
 
-  _onFullscreenPlayerDidDismiss = (event) => {
-    if (this.props.onFullscreenPlayerDidDismiss) {
-      this.props.onFullscreenPlayerDidDismiss(event.nativeEvent);
-    }
-  };
+  // _onFullscreenPlayerDidDismiss = (event) => {
+  //   if (this.props.onFullscreenPlayerDidDismiss) {
+  //     this.props.onFullscreenPlayerDidDismiss(event.nativeEvent);
+  //   }
+  // };
 
-  _onReadyForDisplay = (event) => {
-    if (this.props.onReadyForDisplay) {
-      this.props.onReadyForDisplay(event.nativeEvent);
-    }
-  };
+  // _onReadyForDisplay = (event) => {
+  //   if (this.props.onReadyForDisplay) {
+  //     this.props.onReadyForDisplay(event.nativeEvent);
+  //   }
+  // };
 
-  _onPlaybackStalled = (event) => {
-    if (this.props.onPlaybackStalled) {
-      this.props.onPlaybackStalled(event.nativeEvent);
-    }
-  };
+  // _onPlaybackStalled = (event) => {
+  //   if (this.props.onPlaybackStalled) {
+  //     this.props.onPlaybackStalled(event.nativeEvent);
+  //   }
+  // };
 
-  _onPlaybackResume = (event) => {
-    if (this.props.onPlaybackResume) {
-      this.props.onPlaybackResume(event.nativeEvent);
-    }
-  };
+  // _onPlaybackResume = (event) => {
+  //   if (this.props.onPlaybackResume) {
+  //     this.props.onPlaybackResume(event.nativeEvent);
+  //   }
+  // };
 
-  _onPlaybackRateChange = (event) => {
-    if (this.state.showPoster && (event.nativeEvent.playbackRate !== 0)) {
-      this.setState({showPoster: false});
-    }
+  // _onPlaybackRateChange = (event) => {
+  //   if (this.state.showPoster && (event.nativeEvent.playbackRate !== 0)) {
+  //     this.setState({showPoster: false});
+  //   }
 
-    if (this.props.onPlaybackRateChange) {
-      this.props.onPlaybackRateChange(event.nativeEvent);
-    }
-  };
+  //   if (this.props.onPlaybackRateChange) {
+  //     this.props.onPlaybackRateChange(event.nativeEvent);
+  //   }
+  // };
 
-  _onAudioBecomingNoisy = () => {
-    if (this.props.onAudioBecomingNoisy) {
-      this.props.onAudioBecomingNoisy();
-    }
-  };
+  // _onAudioBecomingNoisy = () => {
+  //   if (this.props.onAudioBecomingNoisy) {
+  //     this.props.onAudioBecomingNoisy();
+  //   }
+  // };
 
-  _onAudioFocusChanged = (event) => {
-    if (this.props.onAudioFocusChanged) {
-      this.props.onAudioFocusChanged(event.nativeEvent);
-    }
-  };
+  // _onAudioFocusChanged = (event) => {
+  //   if (this.props.onAudioFocusChanged) {
+  //     this.props.onAudioFocusChanged(event.nativeEvent);
+  //   }
+  // };
 
-  _onBuffer = (event) => {
-    if (this.props.onBuffer) {
-      this.props.onBuffer(event.nativeEvent);
-    }
-  };
+  // _onBuffer = (event) => {
+  //   if (this.props.onBuffer) {
+  //     this.props.onBuffer(event.nativeEvent);
+  //   }
+  // };
 
   render() {
-    const resizeMode = this.props.resizeMode;
-    const source = resolveAssetSource(this.props.source) || {};
+    // const resizeMode = this.props.resizeMode;
+    // const source = resolveAssetSource(this.props.source) || {};
 
-    let uri = source.uri || '';
-    if (uri && uri.match(/^\//)) {
-      uri = `file://${uri}`;
-    }
+    // let uri = source.uri || '';
+    // if (uri && uri.match(/^\//)) {
+    //   uri = `file://${uri}`;
+    // }
 
-    const isNetwork = !!(uri && uri.match(/^https?:/));
-    const isAsset = !!(uri && uri.match(/^(assets-library|file|content|ms-appx|ms-appdata):/));
+    // const isNetwork = !!(uri && uri.match(/^https?:/));
+    // const isAsset = !!(uri && uri.match(/^(assets-library|file|content|ms-appx|ms-appdata):/));
 
-    let nativeResizeMode;
-    if (resizeMode === VideoResizeMode.stretch) {
-      nativeResizeMode = NativeModules.UIManager.RCTBrightcoveVideo.Constants.ScaleToFill;
-    } else if (resizeMode === VideoResizeMode.contain) {
-      nativeResizeMode = NativeModules.UIManager.RCTBrightcoveVideo.Constants.ScaleAspectFit;
-    } else if (resizeMode === VideoResizeMode.cover) {
-      nativeResizeMode = NativeModules.UIManager.RCTBrightcoveVideo.Constants.ScaleAspectFill;
-    } else {
-      nativeResizeMode = NativeModules.UIManager.RCTBrightcoveVideo.Constants.ScaleNone;
-    }
+    // let nativeResizeMode;
+    // if (resizeMode === VideoResizeMode.stretch) {
+    //   nativeResizeMode = NativeModules.UIManager.RCTBrightcoveVideo.Constants.ScaleToFill;
+    // } else if (resizeMode === VideoResizeMode.contain) {
+    //   nativeResizeMode = NativeModules.UIManager.RCTBrightcoveVideo.Constants.ScaleAspectFit;
+    // } else if (resizeMode === VideoResizeMode.cover) {
+    //   nativeResizeMode = NativeModules.UIManager.RCTBrightcoveVideo.Constants.ScaleAspectFill;
+    // } else {
+    //   nativeResizeMode = NativeModules.UIManager.RCTBrightcoveVideo.Constants.ScaleNone;
+    // }
 
     const nativeProps = Object.assign({}, this.props);
     Object.assign(nativeProps, {
-      style: [styles.base, nativeProps.style],
-      resizeMode: nativeResizeMode,
-      src: {
-        uri,
-        isNetwork,
-        isAsset,
-        type: source.type || '',
-        mainVer: source.mainVer || 0,
-        patchVer: source.patchVer || 0,
-      },
-      onVideoLoadStart: this._onLoadStart,
-      onVideoLoad: this._onLoad,
-      onVideoError: this._onError,
-      onVideoProgress: this._onProgress,
-      onVideoSeek: this._onSeek,
-      onVideoEnd: this._onEnd,
-      onVideoBuffer: this._onBuffer,
-      onVideoFullscreenPlayerWillPresent: this._onFullscreenPlayerWillPresent,
-      onVideoFullscreenPlayerDidPresent: this._onFullscreenPlayerDidPresent,
-      onVideoFullscreenPlayerWillDismiss: this._onFullscreenPlayerWillDismiss,
-      onVideoFullscreenPlayerDidDismiss: this._onFullscreenPlayerDidDismiss,
-      onReadyForDisplay: this._onReadyForDisplay,
-      onPlaybackStalled: this._onPlaybackStalled,
-      onPlaybackResume: this._onPlaybackResume,
-      onPlaybackRateChange: this._onPlaybackRateChange,
-      onAudioFocusChanged: this._onAudioFocusChanged,
-      onAudioBecomingNoisy: this._onAudioBecomingNoisy,
+      style: [nativeProps.style],
+    //   resizeMode: nativeResizeMode,
+    //   src: {
+    //     uri,
+    //     isNetwork,
+    //     isAsset,
+    //     type: source.type || '',
+    //     mainVer: source.mainVer || 0,
+    //     patchVer: source.patchVer || 0,
+    //   },
+    //   onVideoLoadStart: this._onLoadStart,
+    //   onVideoLoad: this._onLoad,
+    //   onVideoError: this._onError,
+    //   onVideoProgress: this._onProgress,
+    //   onVideoSeek: this._onSeek,
+    //   onVideoEnd: this._onEnd,
+    //   onVideoBuffer: this._onBuffer,
+    //   onVideoFullscreenPlayerWillPresent: this._onFullscreenPlayerWillPresent,
+    //   onVideoFullscreenPlayerDidPresent: this._onFullscreenPlayerDidPresent,
+    //   onVideoFullscreenPlayerWillDismiss: this._onFullscreenPlayerWillDismiss,
+    //   onVideoFullscreenPlayerDidDismiss: this._onFullscreenPlayerDidDismiss,
+    //   onReadyForDisplay: this._onReadyForDisplay,
+    //   onPlaybackStalled: this._onPlaybackStalled,
+    //   onPlaybackResume: this._onPlaybackResume,
+    //   onPlaybackRateChange: this._onPlaybackRateChange,
+    //   onAudioFocusChanged: this._onAudioFocusChanged,
+    //   onAudioBecomingNoisy: this._onAudioBecomingNoisy,
     });
 
-    if (this.props.poster && this.state.showPoster) {
-      const posterStyle = {
-        position: 'absolute',
-        left: 0,
-        top: 0,
-        right: 0,
-        bottom: 0,
-        resizeMode: 'contain',
-      };
+    // if (this.props.poster && this.state.showPoster) {
+    //   const posterStyle = {
+    //     position: 'absolute',
+    //     left: 0,
+    //     top: 0,
+    //     right: 0,
+    //     bottom: 0,
+    //     resizeMode: 'contain',
+    //   };
 
       return (
         <View style={nativeProps.style}>
           <RCTBrightcoveVideo
             ref={this._assignRoot}
+            play={this.props.play}
+            autoAdvance={this.props.autoAdvance}
+            autoPlay={this.props.autoPlay}
             video={this.props.video}
             {...nativeProps}
           />
-          <Image
-            style={posterStyle}
-            source={{uri: this.props.poster}}
-          />
+
         </View>
       );
     }
 
-    return (
-      <RCTBrightcoveVideo
-        ref={this._assignRoot}
-        {...nativeProps}
-      />
-    );
-  }
+  //   return (
+  //     <RCTBrightcoveVideo
+  //       ref={this._assignRoot}
+  //       {...nativeProps}
+  //     />
+  //   );
+  // }
 }
 
 BrightcoveVideo.propTypes = {
@@ -253,73 +253,67 @@ BrightcoveVideo.propTypes = {
   }}
   */
 
-  src: PropTypes.object,
-  seek: PropTypes.number,
-  fullscreen: PropTypes.bool,
-  onVideoLoadStart: PropTypes.func,
-  onVideoLoad: PropTypes.func,
-  onVideoBuffer: PropTypes.func,
-  onVideoError: PropTypes.func,
-  onVideoProgress: PropTypes.func,
-  onVideoSeek: PropTypes.func,
-  onVideoEnd: PropTypes.func,
-  onVideoFullscreenPlayerWillPresent: PropTypes.func,
-  onVideoFullscreenPlayerDidPresent: PropTypes.func,
-  onVideoFullscreenPlayerWillDismiss: PropTypes.func,
-  onVideoFullscreenPlayerDidDismiss: PropTypes.func,
+  // src: PropTypes.object,
+  // seek: PropTypes.number,
+  // fullscreen: PropTypes.bool,
+  // onVideoLoadStart: PropTypes.func,
+  // onVideoLoad: PropTypes.func,
+  // onVideoBuffer: PropTypes.func,
+  // onVideoError: PropTypes.func,
+  // onVideoProgress: PropTypes.func,
+  // onVideoSeek: PropTypes.func,
+  // onVideoEnd: PropTypes.func,
+  // onVideoFullscreenPlayerWillPresent: PropTypes.func,
+  // onVideoFullscreenPlayerDidPresent: PropTypes.func,
+  // onVideoFullscreenPlayerWillDismiss: PropTypes.func,
+  // onVideoFullscreenPlayerDidDismiss: PropTypes.func,
 
-  /* Wrapper component */
-  source: PropTypes.oneOfType([
-    PropTypes.shape({
-      uri: PropTypes.string
-    }),
-    // Opaque type returned by require('./video.mp4')
-    PropTypes.number
-  ]),
-  resizeMode: PropTypes.string,
-  poster: PropTypes.string,
-  repeat: PropTypes.bool,
-  paused: PropTypes.bool,
-  muted: PropTypes.bool,
-  volume: PropTypes.number,
-  rate: PropTypes.number,
-  playInBackground: PropTypes.bool,
-  playWhenInactive: PropTypes.bool,
-  disableFocus: PropTypes.bool,
-  controls: PropTypes.bool,
-  currentTime: PropTypes.number,
-  progressUpdateInterval: PropTypes.number,
-  onLoadStart: PropTypes.func,
-  onLoad: PropTypes.func,
-  onBuffer: PropTypes.func,
-  onError: PropTypes.func,
-  onProgress: PropTypes.func,
-  onSeek: PropTypes.func,
-  onEnd: PropTypes.func,
-  onFullscreenPlayerWillPresent: PropTypes.func,
-  onFullscreenPlayerDidPresent: PropTypes.func,
-  onFullscreenPlayerWillDismiss: PropTypes.func,
-  onFullscreenPlayerDidDismiss: PropTypes.func,
-  onReadyForDisplay: PropTypes.func,
-  onPlaybackStalled: PropTypes.func,
-  onPlaybackResume: PropTypes.func,
-  onPlaybackRateChange: PropTypes.func,
-  onAudioFocusChanged: PropTypes.func,
-  onAudioBecomingNoisy: PropTypes.func,
+  // /* Wrapper component */
+  // source: PropTypes.oneOfType([
+  //   PropTypes.shape({
+  //     uri: PropTypes.string
+  //   }),
+  //   // Opaque type returned by require('./video.mp4')
+  //   PropTypes.number
+  // ]),
+  // resizeMode: PropTypes.string,
+  // poster: PropTypes.string,
+  // repeat: PropTypes.bool,
+  // paused: PropTypes.bool,
+  // muted: PropTypes.bool,
+  // volume: PropTypes.number,
+  // rate: PropTypes.number,
+  // playInBackground: PropTypes.bool,
+  // playWhenInactive: PropTypes.bool,
+  // disableFocus: PropTypes.bool,
+  // controls: PropTypes.bool,
+  // currentTime: PropTypes.number,
+  // progressUpdateInterval: PropTypes.number,
+  // onLoadStart: PropTypes.func,
+  // onLoad: PropTypes.func,
+  // onBuffer: PropTypes.func,
+  // onError: PropTypes.func,
+  // onProgress: PropTypes.func,
+  // onSeek: PropTypes.func,
+  // onEnd: PropTypes.func,
+  // onFullscreenPlayerWillPresent: PropTypes.func,
+  // onFullscreenPlayerDidPresent: PropTypes.func,
+  // onFullscreenPlayerWillDismiss: PropTypes.func,
+  // onFullscreenPlayerDidDismiss: PropTypes.func,
+  // onReadyForDisplay: PropTypes.func,
+  // onPlaybackStalled: PropTypes.func,
+  // onPlaybackResume: PropTypes.func,
+  // onPlaybackRateChange: PropTypes.func,
+  // onAudioFocusChanged: PropTypes.func,
+  // onAudioBecomingNoisy: PropTypes.func,
 
-  /* Required by react-native */
-  scaleX: PropTypes.number,
-  scaleY: PropTypes.number,
-  translateX: PropTypes.number,
-  translateY: PropTypes.number,
-  rotation: PropTypes.number,
+  // /* Required by react-native */
+  // scaleX: PropTypes.number,
+  // scaleY: PropTypes.number,
+  // translateX: PropTypes.number,
+  // translateY: PropTypes.number,
+  // rotation: PropTypes.number,
   ...View.propTypes,
 };
 
-const RCTBrightcoveVideo = requireNativeComponent('RCTBrightcoveVideo', BrightcoveVideo, {
-  nativeOnly: {
-    src: true,
-    seek: true,
-    fullscreen: true,
-  },
-});
+const RCTBrightcoveVideo = requireNativeComponent('RCTBrightcoveVideo', BrightcoveVideo);
--- ios/RCTBrightcoveVideoManager.m
+++ ios/RCTBrightcoveVideoManager.m
@@ -2,9 +2,13 @@
 #import "RCTBrightcoveVideo.h"
 #import <React/RCTBridge.h>
 
-@import GoogleInteractiveMediaAds;
-@import BrightcovePlayerSDK;
-@import BrightcoveIMA;
+//@import BrightcovePlayerSDK
+//@import BrightcoveIMA
+
+//
+#import <BrightcovePlayerSDK/BrightcovePlayerSDK.h>
+#import <BrightcoveIMA/BrightcoveIMA.h>
+//#import <GoogleInteractiveMediaAds/GoogleInteractiveMediaAds.h>
 
 @implementation RCTBrightcoveVideoManager
 
@@ -28,46 +32,46 @@ RCT_EXPORT_VIEW_PROPERTY(play, BOOL);
 RCT_EXPORT_VIEW_PROPERTY(autoPlay, BOOL);
 RCT_EXPORT_VIEW_PROPERTY(autoAdvance, BOOL);
 
-#pragma mark React Native Video Props
-RCT_EXPORT_VIEW_PROPERTY(src, NSDictionary);
-RCT_EXPORT_VIEW_PROPERTY(resizeMode, NSString);
-RCT_EXPORT_VIEW_PROPERTY(repeat, BOOL);
-RCT_EXPORT_VIEW_PROPERTY(paused, BOOL);
-RCT_EXPORT_VIEW_PROPERTY(muted, BOOL);
-RCT_EXPORT_VIEW_PROPERTY(controls, BOOL);
-RCT_EXPORT_VIEW_PROPERTY(volume, float);
-RCT_EXPORT_VIEW_PROPERTY(playInBackground, BOOL);
-RCT_EXPORT_VIEW_PROPERTY(playWhenInactive, BOOL);
-RCT_EXPORT_VIEW_PROPERTY(rate, float);
-RCT_EXPORT_VIEW_PROPERTY(seek, float);
-RCT_EXPORT_VIEW_PROPERTY(currentTime, float);
-RCT_EXPORT_VIEW_PROPERTY(fullscreen, BOOL);
-RCT_EXPORT_VIEW_PROPERTY(progressUpdateInterval, float);
-/* Should support: onLoadStart, onLoad, and onError to stay consistent with Image */
-RCT_EXPORT_VIEW_PROPERTY(onVideoLoadStart, RCTBubblingEventBlock);
-RCT_EXPORT_VIEW_PROPERTY(onVideoLoad, RCTBubblingEventBlock);
-RCT_EXPORT_VIEW_PROPERTY(onVideoBuffer, RCTBubblingEventBlock);
-RCT_EXPORT_VIEW_PROPERTY(onVideoError, RCTBubblingEventBlock);
-RCT_EXPORT_VIEW_PROPERTY(onVideoProgress, RCTBubblingEventBlock);
-RCT_EXPORT_VIEW_PROPERTY(onVideoSeek, RCTBubblingEventBlock);
-RCT_EXPORT_VIEW_PROPERTY(onVideoEnd, RCTBubblingEventBlock);
-RCT_EXPORT_VIEW_PROPERTY(onVideoFullscreenPlayerWillPresent, RCTBubblingEventBlock);
-RCT_EXPORT_VIEW_PROPERTY(onVideoFullscreenPlayerDidPresent, RCTBubblingEventBlock);
-RCT_EXPORT_VIEW_PROPERTY(onVideoFullscreenPlayerWillDismiss, RCTBubblingEventBlock);
-RCT_EXPORT_VIEW_PROPERTY(onVideoFullscreenPlayerDidDismiss, RCTBubblingEventBlock);
-RCT_EXPORT_VIEW_PROPERTY(onReadyForDisplay, RCTBubblingEventBlock);
-RCT_EXPORT_VIEW_PROPERTY(onPlaybackStalled, RCTBubblingEventBlock);
-RCT_EXPORT_VIEW_PROPERTY(onPlaybackResume, RCTBubblingEventBlock);
-RCT_EXPORT_VIEW_PROPERTY(onPlaybackRateChange, RCTBubblingEventBlock);
+//#pragma mark React Native Video Props
+//RCT_EXPORT_VIEW_PROPERTY(src, NSDictionary);
+//RCT_EXPORT_VIEW_PROPERTY(resizeMode, NSString);
+//RCT_EXPORT_VIEW_PROPERTY(repeat, BOOL);
+//RCT_EXPORT_VIEW_PROPERTY(paused, BOOL);
+//RCT_EXPORT_VIEW_PROPERTY(muted, BOOL);
+//RCT_EXPORT_VIEW_PROPERTY(controls, BOOL);
+//RCT_EXPORT_VIEW_PROPERTY(volume, float);
+//RCT_EXPORT_VIEW_PROPERTY(playInBackground, BOOL);
+//RCT_EXPORT_VIEW_PROPERTY(playWhenInactive, BOOL);
+//RCT_EXPORT_VIEW_PROPERTY(rate, float);
+//RCT_EXPORT_VIEW_PROPERTY(seek, float);
+//RCT_EXPORT_VIEW_PROPERTY(currentTime, float);
+//RCT_EXPORT_VIEW_PROPERTY(fullscreen, BOOL);
+//RCT_EXPORT_VIEW_PROPERTY(progressUpdateInterval, float);
+///* Should support: onLoadStart, onLoad, and onError to stay consistent with Image */
+//RCT_EXPORT_VIEW_PROPERTY(onVideoLoadStart, RCTBubblingEventBlock);
+//RCT_EXPORT_VIEW_PROPERTY(onVideoLoad, RCTBubblingEventBlock);
+//RCT_EXPORT_VIEW_PROPERTY(onVideoBuffer, RCTBubblingEventBlock);
+//RCT_EXPORT_VIEW_PROPERTY(onVideoError, RCTBubblingEventBlock);
+//RCT_EXPORT_VIEW_PROPERTY(onVideoProgress, RCTBubblingEventBlock);
+//RCT_EXPORT_VIEW_PROPERTY(onVideoSeek, RCTBubblingEventBlock);
+//RCT_EXPORT_VIEW_PROPERTY(onVideoEnd, RCTBubblingEventBlock);
+//RCT_EXPORT_VIEW_PROPERTY(onVideoFullscreenPlayerWillPresent, RCTBubblingEventBlock);
+//RCT_EXPORT_VIEW_PROPERTY(onVideoFullscreenPlayerDidPresent, RCTBubblingEventBlock);
+//RCT_EXPORT_VIEW_PROPERTY(onVideoFullscreenPlayerWillDismiss, RCTBubblingEventBlock);
+//RCT_EXPORT_VIEW_PROPERTY(onVideoFullscreenPlayerDidDismiss, RCTBubblingEventBlock);
+//RCT_EXPORT_VIEW_PROPERTY(onReadyForDisplay, RCTBubblingEventBlock);
+//RCT_EXPORT_VIEW_PROPERTY(onPlaybackStalled, RCTBubblingEventBlock);
+//RCT_EXPORT_VIEW_PROPERTY(onPlaybackResume, RCTBubblingEventBlock);
+//RCT_EXPORT_VIEW_PROPERTY(onPlaybackRateChange, RCTBubblingEventBlock);
 
-- (NSDictionary *)constantsToExport
-{
-  return @{
-    @"ScaleNone": AVLayerVideoGravityResizeAspect,
-    @"ScaleToFill": AVLayerVideoGravityResize,
-    @"ScaleAspectFit": AVLayerVideoGravityResizeAspect,
-    @"ScaleAspectFill": AVLayerVideoGravityResizeAspectFill
-  };
-}
+//- (NSDictionary *)constantsToExport
+//{
+//  return @{
+//    @"ScaleNone": AVLayerVideoGravityResizeAspect,
+//    @"ScaleToFill": AVLayerVideoGravityResize,
+//    @"ScaleAspectFit": AVLayerVideoGravityResizeAspect,
+//    @"ScaleAspectFill": AVLayerVideoGravityResizeAspectFill
+//  };
+//}
 
 @end
